a5677955f5e18597396a73d91e45f8b514636165
ac-optimus <abhavya.chandra@iitgn.ac.in>
2018-12-13 18:38:50 +0530

    creating workspace

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/ac-optimus/Desktop/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/ac-optimus/Desktop/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/ac-optimus/Desktop/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/ac-optimus/Desktop/catkin_ws/src:$ROS_PACKAGE_PATH"
