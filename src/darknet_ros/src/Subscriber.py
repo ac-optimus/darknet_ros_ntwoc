de4bc9ebcafa6dd97fab3c0575e0ed7aac67ca93
ac-optimus <abhavya.chandra@iitgn.ac.in>
2018-12-13 19:22:33 +0530

    correcting error in SUb

--- src/darknet_ros/src/Subscriber.py
+++ src/darknet_ros/src/Subscriber.py
@@ -6,7 +6,7 @@ def callback(data):
     rospy.loginfo((data.Class,data.probability,data.xmin,data.ymin,data.xmax,data.ymax))
 
 
-def SubsCriber()
+def SubsCriber():
     #initialize the node
     rospy.init_node("Reciever",anonymous = True)
     rospy.Subscriber("CoOrdinates", BoundingBox,callback)
