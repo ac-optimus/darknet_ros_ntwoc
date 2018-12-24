f9d00697b36ff18bd04b904f15770a1d0d090036
ac-optimus <abhavya.chandra@iitgn.ac.in>
2018-12-24 11:44:59 +0530

    adding darknet source files

---
+++ src/topicImgThreadcpp/src/image_detectOnly.cpp
@@ -0,0 +1,100 @@
+#include <iostream>
+#include <iomanip> 
+#include <string>
+#include <vector>
+#include <queue>
+#include <fstream>
+#include <thread>
+#include <atomic>
+#include <mutex>              // std::mutex, std::unique_lock
+#include <condition_variable> // std::condition_variable
+#include <unistd.h>
+
+#define MY_PATH "/home/ac-optimus/Pictures/darknet" //working directory
+
+//opencv
+#include <opencv2/opencv.hpp>            // C++
+#include "opencv2/core/version.hpp"
+#ifndef CV_VERSION_EPOCH
+#include "opencv2/videoio/videoio.hpp"
+#define OPENCV_VERSION CVAUX_STR(CV_VERSION_MAJOR)"" CVAUX_STR(CV_VERSION_MINOR)"" CVAUX_STR(CV_VERSION_REVISION)
+#pragma comment(lib, "opencv_world" OPENCV_VERSION ".lib")
+
+#else
+#define OPENCV_VERSION CVAUX_STR(CV_VERSION_EPOCH)""CVAUX_STR(CV_VERSION_MAJOR)""CVAUX_STR(CV_VERSION_MINOR)
+#pragma comment(lib, "opencv_core" OPENCV_VERSION ".lib")
+#pragma comment(lib, "opencv_imgproc" OPENCV_VERSION ".lib")
+#pragma comment(lib, "opencv_highgui" OPENCV_VERSION ".lib")
+#endif    // CV_VERSION_EPOCH
+
+
+#include "yolo_v2_class.hpp"
+//I have added them
+std::vector<std::string> objects_names_from_file(std::string const filename) {
+    std::ifstream file(filename);
+    std::vector<std::string> file_lines;
+    if (!file.is_open()) return file_lines;
+    for(std::string line; getline(file, line);) file_lines.push_back(line);
+    std::cout << "object names loaded \n";
+    return file_lines;
+}
+void draw_boxes(cv::Mat mat_img, std::vector<bbox_t> result_vec, std::vector<std::string> obj_names, 
+    int current_det_fps = -1, int current_cap_fps = -1)
+{
+    std::cout<<"I was here"<<std::endl;
+    int const colors[6][3] = { { 1,0,1 },{ 0,0,1 },{ 0,1,1 },{ 0,1,0 },{ 1,1,0 },{ 1,0,0 } };
+
+    for (auto &i : result_vec) {
+        cv::Scalar color = obj_id_to_color(i.obj_id);
+        cv::rectangle(mat_img, cv::Rect(i.x, i.y, i.w, i.h), color, 2);
+        if (obj_names.size() > i.obj_id) {
+            std::string obj_name = obj_names[i.obj_id];
+            if (i.track_id > 0) obj_name += " - " + std::to_string(i.track_id);
+            cv::Size const text_size = getTextSize(obj_name, cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, 2, 0);
+            int const max_width = (text_size.width > i.w + 2) ? text_size.width : (i.w + 2);
+            cv::rectangle(mat_img, cv::Point2f(std::max((int)i.x - 1, 0), std::max((int)i.y - 30, 0)), 
+                cv::Point2f(std::min((int)i.x + max_width, mat_img.cols-1), std::min((int)i.y, mat_img.rows-1)), 
+                color, CV_FILLED, 8, 0);
+            putText(mat_img, obj_name, cv::Point2f(i.x, i.y - 10), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, cv::Scalar(0, 0, 0), 2);
+        }
+    }
+    if (current_det_fps >= 0 && current_cap_fps >= 0) {
+        std::string fps_str = "FPS detection: " + std::to_string(current_det_fps) + "   FPS capture: " + std::to_string(current_cap_fps);
+        putText(mat_img, fps_str, cv::Point2f(10, 20), cv::FONT_HERSHEY_COMPLEX_SMALL, 1.2, cv::Scalar(50, 255, 0), 2);
+    }
+}
+
+
+
+
+void print_result(std::vector<bbox_t>result_vec)
+{
+    std::vector<bbox_t> l =result_vec;
+    
+    for (auto i=l.begin(); i != l.end(); ++i )
+    std::cout << "result details" << " " <<(*i).obj_id << " " << (*i).x<< " " << (*i).y << " " << (*i).w << " " << (*i).h<< " " << std::endl; 
+
+}
+
+
+
+int main(int argc, char** argv)
+{
+    //for image file
+    chdir(MY_PATH);
+    std::string  names_file = "data/coco.names";   //add the data file here
+    std::string  cfg_file = "cfg/yolov3-tiny.cfg";  //configuration file
+    std::string  weights_file = "yolov3-tiny.weights"; //weight files
+    float const thresh = 0.20;
+
+    auto obj_name = objects_names_from_file(names_file); //a vector of strings
+    Detector detector(cfg_file, weights_file);
+    cv::Mat img = cv::imread(argv[1]);//argv[1] has to be the file name.
+    std::vector<bbox_t> msgBox = detector.detect(img);
+    print_result(msgBox);
+    draw_boxes(img,msgBox,obj_name);
+    cv::imshow("Output",img);
+    cv::waitKey(0);
+    std::cout<<"tada"<<std::endl;
+    return 0;
+}
